aeec8d9d68273c37cca0db11dba6731fd4da289e
stringcode <michael@stringcode.co.uk>
2024-10-25 21:09:25 +0300

    Fixing layout

--- react-native-ios-android-web/packages/app/utils/smallCardSize.ts
+++ react-native-ios-android-web/packages/app/utils/smallCardSize.ts
@@ -2,9 +2,10 @@ export function smallCardSizeAndColCnt(
   container: number,
   pad: number
 ): { width: number; colCount: number } {
-  const w = (container > 600 ? 300 : container / 2) - pad;
-  return {
-    width: w,
-    colCount: Math.max(2, Math.floor(container / w)),
-  };
+  let w = Math.min(275, (container / 2) - pad)
+  let cols = Math.max(2, Math.floor(container / w))
+  if (w > 209) {
+    w = Math.min(275, (container - pad - pad * cols) / cols)
+  }
+  return { width: w, colCount: cols }
 }
