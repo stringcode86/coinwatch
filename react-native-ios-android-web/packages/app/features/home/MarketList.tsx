6ab311119bd54538f8e79d6df5777c94d417a2a4
stringcode <michael@stringcode.co.uk>
2024-10-25 17:09:07 +0300

    Adding chart

--- react-native-ios-android-web/packages/app/features/home/MarketList.tsx
+++ react-native-ios-android-web/packages/app/features/home/MarketList.tsx
@@ -1,12 +1,15 @@
-import { getTokenValue } from '@tamagui/core'
+import { getTokenValue, useTheme } from '@tamagui/core'
 import { useState, memo, useMemo } from 'react'
 import { FlatList, type LayoutChangeEvent } from 'react-native'
 import { Market } from 'app/data/coinGeckoClient/types'
 import { smallCardSizeAndColCnt } from 'app/utils/smallCardSize'
-import { SizableText, Card, XStack, YStack, Image, View } from "@my/ui";
+import { SizableText, Card, XStack, YStack, Image, View} from "@my/ui";
+import { LinearGradient } from '@tamagui/linear-gradient'
 import { useRouter } from "solito/navigation";
 import { NumericFormat } from 'react-number-format';
 import { abbreviateNumber } from 'app/utils/abbreviateNumber'
+import { LineChart, Grid } from 'react-native-svg-charts'
+
 
 type MarketCardProps = {
   market: Market,
@@ -19,6 +22,11 @@ const MarketCard = memo(({market, width = 100, onPress }: MarketCardProps) => {
   const pad = sm ? "$3" : "$4"
   const fs = sm ? "$5" : "$7"
   const fssm = sm ? "$3" : "$5"
+
+  const up = useMemo(
+    () => (market.price_change_percentage_24h ?? 1) > 0,
+    [market.price_change_percentage_24h]
+  )
 
   const marketCap = useMemo(
     () => abbreviateNumber(market.market_cap ?? ''),
@@ -35,6 +43,9 @@ const MarketCard = memo(({market, width = 100, onPress }: MarketCardProps) => {
     [market.price_change_percentage_24h]
   )
 
+  const theme = useTheme()
+  const chartColor = up ? theme.green10.get() : theme.red10.get()
+
   return (
     <Card
       w={width}
@@ -42,49 +53,59 @@ const MarketCard = memo(({market, width = 100, onPress }: MarketCardProps) => {
       hoverStyle={{scale: 0.925}}
       pressStyle={{scale: 0.975}}
       animation="bouncy"
-      p={sm ? "$3" : "$4"}
+      p={pad}
       elevate
       onPress={onPress}
     >
-        <XStack gap={pad}>
-          <YStack justifyContent="space-between">
-            <Image source={{ uri: market.image }} w={pad} aspectRatio={1} />
-            <NumericFormat
-              displayType="text"
-              value={market.price_change_percentage_24h ?? ''}
-              suffix={'%'}
-              decimalScale={2}
-              renderText={(value) => <SizableText size={fssm}>{value}</SizableText>}
-            />
-          </YStack>
-          <YStack f={1}>
-            <SizableText size={fs} fow="600" numberOfLines={1}>{market.name}</SizableText>
-            <SizableText size={fssm}>cap { marketCap }</SizableText>
-            <SizableText size={fssm}>vol { volume }</SizableText>
-          </YStack>
-        </XStack>
+      <Card.Background overflow={'hidden'} paddingTop="70%">
+        <LinearGradient
+          width="100%"
+          height="100%"
+          borderRadius="$4"
+          colors={[ up ? '$green4' : '$red4', '$background']}
+          start={[0, 1]}
+          end={[0, 0]}
+        />
+      </Card.Background>
+
+      <XStack gap={pad}>
+        <YStack justifyContent="space-between">
+          <Image source={{ uri: market.image }} w={pad} aspectRatio={1} />
+          <SizableText size={fssm} col={up ? '$green10' : '$red11'}>{pricePct}</SizableText>
+        </YStack>
+        <YStack f={1}>
+          <SizableText size={fs} fow="600" numberOfLines={1}>{market.name}</SizableText>
+          <SizableText size={fssm}>cap { marketCap }</SizableText>
+          <SizableText size={fssm}>vol { volume }</SizableText>
+        </YStack>
+      </XStack>
 
       <View f={1}>
-        <SizableText> </SizableText>
+        <LineChart
+          data={market.sparkline_in_7d?.price ?? []}
+          style={{height: '100%'}}
+          svg={{ stroke: chartColor, strokeWidth: 2 }}
+          contentInset={{ top: sm ? 8 : 16, bottom: sm ? 4 : 8 }}
+        />
       </View>
-
-      <Card.Background>
-
-      </Card.Background>
 
       <Card.Footer>
         <XStack justifyContent="center" f={1}>
+          <SizableText size={fs} fow="700" col={up ? '$green10' : '$red11'} >{up ? '▲ ' : '▼ '}</SizableText>
           <NumericFormat
             displayType="text"
             value={market.current_price}
             thousandsGroupStyle="lakh"
             thousandSeparator=","
-            prefix={`▲ $`}
+            prefix={'$'}
             decimalScale={2} // TODO: Handle price 0.0000123
-            renderText={(value) => <SizableText size={fs} fow="700">{value}</SizableText>}
+            renderText={(value) =>
+              <SizableText size={fs} fow="700">{value}</SizableText>
+            }
           />
         </XStack>
       </Card.Footer>
+
     </Card>
   )
 })
